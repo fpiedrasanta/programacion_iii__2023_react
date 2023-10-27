982eded07955e6e060f42ceb21ee0fe3640fb220
Federico Piedrasanta <fede.piedrasanta@gmail.com>
2023-10-26 23:46:06 -0300

    26/10/2023

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { NavBar } from './components/NavBar/navBar';
+import { MyButton } from './components/MyButton/myButton';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <MyButton />
+    </>
+  );
+}
+
+export default App;
